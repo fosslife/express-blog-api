0db7e67b4c0a6a58005eb5f48919eb7ec5ea481b
Sparkenstein <zetabytes.pp@gmail.com>
2019-04-14 17:46:48 +0530

    still WIP

--- routes/post.js
+++ routes/post.js
@@ -3,7 +3,7 @@ const db = require('../db');
 const validateRequest = require('../utils/validateRequests');
 
 r.post('/', (req, res, next) => {
-    
+
 }, async (req, res) => {
     const payload = req.body;
     await db.insert(payload);
@@ -22,15 +22,18 @@ r.get('/author/:author', async (req, res) => {
     res.json(doc);
 });
 
+r.get('/alltags', (req, res) => {
+    console.log("All")
+    res.end("STUFF")
+    // const alldocs = await db.findAll();
+    // console.log(typeof alldocs);
+    // res.json(alldocs);
+})
+
 r.get('/tag/:tag', async (req, res) => {
     const tags = req.params.tag;
-    console.log(tags);
-    if(tags){
-        const doc = await db.find({ tags: { $in : [tags]} });
-    } else {
-        // const doc = await db.findAll() ?
-    }
+    const doc = await db.find({ tags: { $in: [tags] } });
     res.json(doc);
-})
+});
 
 module.exports = r;
