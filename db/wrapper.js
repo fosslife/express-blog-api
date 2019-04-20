2515938f4480b2e4eb50ca4be3f007a70446c657
Sparkenstein <zetabytes.pp@gmail.com>
2019-04-20 15:08:15 +0530

    fetch regardless of spelling case

--- db/wrapper.js
+++ db/wrapper.js
@@ -7,7 +7,7 @@ const getAuthorByName = db.prepare(`SELECT * FROM Author WHERE lower(Name) = ? `
 
 const registerAuthor = db.prepare(`INSERT INTO Author(Name, Bio) VALUES(?, ?);`);
 
-const getAllAuthors = db.prepare(`SELECT DISTINCT(Name) from Author`);
+const getAllAuthors = db.prepare(`SELECT DISTINCT(Name), Bio from Author`);
 
 module.exports = {
     insertPost,
